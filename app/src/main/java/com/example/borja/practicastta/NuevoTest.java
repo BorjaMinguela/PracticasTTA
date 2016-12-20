d0e80fb4810d3d47ee601dbdee074e09b5a9b17d
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-12-20 19:01:54 +0100

    Añadida otra vista y comienzo de la segunda

---
+++ app/src/main/java/com/example/borja/practicastta/NuevoTest.java
@@ -0,0 +1,13 @@
+package com.example.borja.practicastta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NuevoTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_test);
+    }
+}
