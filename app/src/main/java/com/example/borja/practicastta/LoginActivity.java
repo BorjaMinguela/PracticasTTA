ff3db935b2ea00b33ba6f8af407f396f2a0689f0
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-12-21 14:40:49 +0100

    Completada la vista del login Modificada la actividad LoginActivity que se creaba por defecto para que no de error

--- app/src/main/java/com/example/borja/practicastta/LoginActivity.java
+++ app/src/main/java/com/example/borja/practicastta/LoginActivity.java
@@ -34,26 +34,34 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+//public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
+  /*  *//**
      * Id to identity READ_CONTACTS permission request.
-     */
+     *//*
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
+    *//**
+     * A dummy authentication store containing known user names and passwords
+     *//*
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
-    /**
+    *//**
      * Keep track of the login task to ensure we can cancel it if requested.
-     */
+     *//*
     private UserLoginTask mAuthTask = null;
 
     // UI references.
@@ -124,9 +132,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return false;
     }
 
-    /**
+    *//**
      * Callback received when a permissions request has been completed.
-     */
+     *//*
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
@@ -138,11 +146,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
 
-    /**
+    *//**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
-     */
+     *//*
     private void attemptLogin() {
         if (mAuthTask != null) {
             return;
@@ -200,9 +208,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return password.length() > 4;
     }
 
-    /**
+    *//**
      * Shows the progress UI and hides the login form.
-     */
+     *//*
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
@@ -290,10 +298,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
-    /**
+    *//**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
-     */
+     *//*
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
@@ -345,6 +353,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
         }
-    }
-}
+    }*/
+//}
 
