18d62594e0c68be393c3ffc1b79553df6bbf84d1
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-12-23 14:54:23 +0100

    Avanzado el metodo corregir Aun no esta acabado, problema con los ids de los radiobuttons

--- app/src/main/java/com/example/borja/practicastta/NuevoTest.java
+++ app/src/main/java/com/example/borja/practicastta/NuevoTest.java
@@ -1,8 +1,10 @@
 package com.example.borja.practicastta;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -33,6 +35,21 @@ public class NuevoTest extends AppCompatActivity implements View.OnClickListener
     }
 
     public void corregir(View view){
-        Toast.makeText(getApplicationContext(),"Corregir!", Toast.LENGTH_SHORT).show();
+        int correct=4;
+
+        RadioGroup group = (RadioGroup)findViewById(R.id.test_choices);
+        View selected = group.findViewById(group.getCheckedRadioButtonId());
+        View correctOption=group.getChildAt(correct);
+        int choices = group.getChildCount();
+        for (int i=0;i<choices;i++){
+            group.getChildAt(i).setEnabled(false);
+        }
+        LinearLayout layout = (LinearLayout)findViewById(R.id.activity_nuevo_test);
+        layout.removeView(findViewById(R.id.button_sendTest));
+        group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+        if (selected.equals(correctOption)){
+            selected.setBackgroundColor(Color.RED);
+        }
+        Toast.makeText(getApplicationContext(),"Corregir", Toast.LENGTH_SHORT).show();
     }
 }
