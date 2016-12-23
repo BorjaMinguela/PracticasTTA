f2c4c2d38d002dbc6ea8505f755ee253103ecfe7
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-12-23 14:14:13 +0100

    Se crea una de las opciones desde el codigo Se ha añadido el metodo onclick

--- app/src/main/java/com/example/borja/practicastta/NuevoTest.java
+++ app/src/main/java/com/example/borja/practicastta/NuevoTest.java
@@ -3,16 +3,36 @@ package com.example.borja.practicastta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class NuevoTest extends AppCompatActivity {
+import com.example.borja.practicastta.model.Test;
+
+public class NuevoTest extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_test);
+
+        Test test=new Test();
+        test.setWording("La buena ;)");
+        RadioGroup group = (RadioGroup)findViewById(R.id.test_choices);
+        RadioButton radio = new RadioButton(this);
+        radio.setText(test.getWording());
+        radio.setOnClickListener(this);
+        group.addView(radio);
+        Toast.makeText(getApplicationContext(),"Creado!", Toast.LENGTH_SHORT).show();
+
     }
-//    @Override
-//    public void onClick(View v){
-//        findViewById(R.id.button_sendTest).setVisibility(View.VISIBLE);
-//    }
+    @Override
+    public void onClick(View v){
+        Toast.makeText(getApplicationContext(),"onCLick!", Toast.LENGTH_SHORT).show();
+        findViewById(R.id.button_sendTest).setVisibility(View.VISIBLE);
+    }
+
+    public void corregir(View view){
+        Toast.makeText(getApplicationContext(),"Corregir!", Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/example/borja/practicastta/model/Test.java
@@ -0,0 +1,16 @@
+package com.example.borja.practicastta.model;
+
+/**
+ * Created by borja on 23/12/16.
+ */
+
+public class Test {
+    private String wording;
+
+    public String getWording(){
+        return this.wording;
+    }
+    public void setWording(String wording){
+        this.wording=wording;
+    }
+}
