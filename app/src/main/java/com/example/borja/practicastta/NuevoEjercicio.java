6e0a0cb8a192d7535136d5f279203ce27860dd5b
Borja Minguela <bminguela001@ikasle.ehu.eus>
2016-12-27 18:46:02 +0100

    Añadido el método para sacar fotos

--- app/src/main/java/com/example/borja/practicastta/NuevoEjercicio.java
+++ app/src/main/java/com/example/borja/practicastta/NuevoEjercicio.java
@@ -1,13 +1,47 @@
 package com.example.borja.practicastta;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class NuevoEjercicio extends AppCompatActivity {
+    final int READ_REQUEST_CODE=1;
+    final int VIDEO_REQUEST_CODE=2;
+    final int AUDIO_REQUEST_CODE=3;
+    final int PICTURE_REQUEST_CODE=4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_ejercicio);
+    }
+    public void sacarFoto(View view){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            Toast.makeText(this,R.string.no_camera,Toast.LENGTH_SHORT).show();
+        else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())!=null){
+                File dir= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                try{
+                    File file=File.createTempFile("tta",".jpg",dir);
+                    Uri pictureUri= Uri.fromFile(file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,pictureUri);
+                    startActivityForResult(intent,PICTURE_REQUEST_CODE);
+                }catch (IOException e){
+
+                }
+            }
+            else
+                Toast.makeText(this, R.string.no_app,Toast.LENGTH_SHORT).show();
+        }
     }
 }
